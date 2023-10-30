cba09345de9ee4b75d38e1a588b7dbdca257fb2f
qsChe <flllryh@gmail.com>
2023-10-30 11:51:09 +0800

    网络科学作业目录建立

---
+++ network_science/main.py
@@ -0,0 +1,16 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def generate_matrix(N, p):
+    matrix = np.zeros((N, N))
+    return matrix
+
+
+if __name__ == '__main__':
+    # initial
+    N = 1e2  # number of nodes, 1e2 / 1e3 / 2e3
+    p = 1e-5  # possibility of connect between two nodes
+    # generate nodes and edge
+
+    # plot figure and save data
